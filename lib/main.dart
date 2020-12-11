04ff593232351dfa668b2909c200e0235a1f96cc
Suyog Subedi <subedisuyog555@gmail.com>
2020-12-11 13:48:53 +0545

    ID card stateless

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('ID'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/suyog.jpg'),
+                radius: 80.0,
+              ),
+            ),
+            // Adds a line
+            Divider(
+              height: 80.0,
+              color: Colors.grey[800],
+            ),
+            Text('NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Suyog Subedi',
+                style: TextStyle(
+                    color: Colors.amber[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 25,
+            ),
+            Text('Current Education Level',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Bachelor In Computing',
+                style: TextStyle(
+                    color: Colors.amber[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'subsuyog@gmail.com',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
